3be051451587fe1590f95270432eff822428cdd6
Antonio Sejas <antonio@sejas.es>
2015-10-16 10:52:53 +0100

    Google login working, and firebase dependencies installed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,40 @@
 angular.module('starter.controllers', [])
 
+.controller('LoginCtrl', function($scope,$state,UserService, $ionicLoading) {
+  $scope.loginSuccess = function  () {
+    $state.go('tab.dash');
+  }
+
+  $scope.googleSignIn = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
+    });
+
+    window.plugins.googleplus.login(
+      {},
+      function (user_data) {
+        // For the purpose of this example I will store user data on local storage
+        UserService.setUser({
+          userID: user_data.userId,
+          name: user_data.displayName,
+          email: user_data.email,
+          picture: user_data.imageUrl,
+          accessToken: user_data.accessToken,
+          idToken: user_data.idToken
+        });
+
+        $ionicLoading.hide();
+        $scope.loginSuccess();
+      },
+      function (msg) {
+        //Error
+        console.error('Error while google login:',msg);
+        $ionicLoading.hide();
+      }
+    );
+  };//End googleSignIn
+
+})
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
